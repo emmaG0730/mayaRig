f57d069c21dc1c65b2a2bcd36593113ebfa9a0df
emmaG0730 <xiaoyezi2006719@126.com>
2018-09-13 23:10:39 +0800

    add UI

--- code/IKFKSeamlessCreate.py
+++ code/IKFKSeamlessCreate.py
@@ -1,42 +1,57 @@
 import maya.cmds as mc
+import functools
+
+def createUI(armGeneration):	
+	ArmWin = 'AdvancedArmCreation'
+	if mc.window(ArmWin,q=True,exists = True):
+		mc.deleteUI(ArmWin)
+	win = mc.window(ArmWin,t = 'ArmTwist',wh =(1000,500))
+	mc.columnLayout( adjustableColumn=True )
+	inputValue = mc.intField(value= True)
+	mc.button( label='GenerateArm', c = functools.partial(armGeneration,inputValue))
+	mc.text( label='JointAmount' )
+	mc.showWindow(win)
+
 
 
 #inputAmount = 
-for each in range(5):
-	mc.joint( p = (each,0,0))
-	mc.joint(n = 'advanced_' + str(each+1) + '_JNT' , e= True,rad = 0.5,oj='xyz',sao  ='yup')
+def armGeneration(inputValue):
+	input = mc.intField(inputValue,q= True,value= True)
+	print input
+	for each in range(input):
+		mc.joint( p = (each,0,0))
+		mc.joint(n = 'advanced_' + str(each+1) + '_JNT' , e= True,rad = 0.5,oj='xyz',sao  ='yup')
+		
+	#startPosition = (0,0,0)
+	#endPosition = (0,0,4)
 	
-#startPosition = (0,0,0)
-#endPosition = (0,0,4)
+	mc.curve(n='advancedCurve1',p = [(0,0,0),(0,0,input-1)],d =1)
+	mc.ikHandle( sj='advanced_1_JNT', ee='advanced_5_JNT', c ='advancedCurve1' ,ccv = False,scv = False,
+					fj = True,sol = 'ikSplineSolver',tws ="easeIn",roc = True)
+	
+	mc.spaceLocator(n = 'Start')
+	mc.move(0,1,0)
+	mc.spaceLocator(n = 'End')
+	mc.move(input-1,1,0)
+	
+	mc.select('ikHandle1')
+	mc.setAttr ("ikHandle1.dTwistControlEnable" ,1)
+	mc.setAttr ("ikHandle1.dWorldUpType", 2)
+	#mc.setAttr ("ikHandle1.dWorldUpObject", 'Start|StartShape')
+	#mc.setAttr ("ikHandle1.dWorldUpObject2", 'End|EndShape')
+	
+	mc.joint(n = 'joint1Con',p = (0,0,0))
+	mc.joint(n = 'joint2Con',p = (input-1,0,0))
+	
+	degree = mc.getAttr( 'advancedCurve1.degree' )
+	span = mc.getAttr( 'advancedCurve1.spans' )
+	CVAmount = degree + span
+	for eachCV in range(CVAmount):
+		mc.select('advancedCurve1.cv[' + str(eachCV) + ']')
+		mc.cluster(en = 1)
+	
+	mc.parent('Start','cluster1Handle','joint1Con')
+	mc.parent('End','cluster'+str(CVAmount)+'Handle','joint2Con')
 
-mc.curve(n='advancedCurve1',p = [(0,0,0),(0,0,4)],d =1)
-mc.ikHandle( sj='advanced_1_JNT', ee='advanced_5_JNT', c ='advancedCurve1' ,ccv = False,scv = False,
-				fj = True,sol = 'ikSplineSolver',tws ="easeIn",roc = True)
-
-mc.spaceLocator(n = 'Start')
-mc.move(0,1,0)
-mc.spaceLocator(n = 'End')
-mc.move(04,1,0)
-
-#mc.move(0,1,0,loc1[0]+".scalePivot",loc1[0]+".rotatePivot")
-#mc.move(4,1,0,loc2[0]+".scalePivot",loc2[0]+".rotatePivot")
-
-mc.select('ikHandle1')
-mc.setAttr ("ikHandle1.dTwistControlEnable" ,1)
-mc.setAttr ("ikHandle1.dWorldUpType", 2)
-mc.setAttr ("ikHandle1.dWorldUpObject", 'Start|StartShape')
-mc.setAttr ("ikHandle1.dWorldUpObject2", 'End|EndShape')
-
-mc.joint(n = 'joint1Con',p = (0,0,0))
-mc.joint(n = 'joint2Con',p = (4,0,0))
-
-mc.parent('Start','joint1Con')
-mc.parent('End','joint2Con')
-
-degree = mc.getAttr( 'advancedCurve1.degree' )
-span = mc.getAttr( 'advancedCurve1.spans' )
-CVAmount = degree + span
-for eachCV in range(CVAmount):
-	mc.select('advancedCurve1.cv[' + str(eachCV) + ']')
-	mc.cluster(en = 1)
+createUI(armGeneration)
 
