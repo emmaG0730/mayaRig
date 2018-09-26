3d38a9f304c242122233192d35ef5306b8e3ffb9
emmaG0730 <xiaoyezi2006719@126.com>
2018-09-26 20:46:38 +0800

    add ik controller

--- code/OnlyUI.py
+++ code/OnlyUI.py
@@ -1,16 +1,15 @@
 import maya.cmds as mc
 
 class ArmCreation(object):
-	#jointAmounts = 0
+
 	def __init__(self):
 		self.ArmWin = 'AdvancedArmCreation'
 		self.title =  'ArmTwist'
 		self.size = (300,100)			
 		
-	def printOut(self):
-		self.slider = mc.intSliderGrp(l = 'JointAmount',min= 1,max = 10,field= True)
-		self.jointAmounts = mc.intSliderGrp(self.slider,q= True,v=True)
-		print self.jointAmounts
+	def printOut(self, *args, **kwargs):
+		jointAmounts = mc.intSliderGrp(self.slider,q=1,v=1)
+		return jointAmounts
 		
 	def createUI(self):			
 		if mc.window(self.ArmWin,q=True,exists = True):
@@ -18,8 +17,11 @@ class ArmCreation(object):
 		self.win = mc.window(self.ArmWin, t = self.title,wh =self.size,s = False,resizeToFitChildren = True)
 		self.layout = mc.columnLayout(adj = True)		
 		self.slider = mc.intSliderGrp(l = 'JointAmount',min= 1,max = 10,field= True)
-		mc.button(label='GenerateArm',c ='printOut()')	
-		mc.showWindow()	
+		mc.button(label='GenerateArm',c =self.printOut)	
+		#amount = self.printOut()
+		#print amount
+		mc.showWindow(self.win)	
+
 		
 a= ArmCreation()
-a.createUI()
+a. createUI()
