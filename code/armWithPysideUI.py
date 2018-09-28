c83ca692101eac269fe58c2f13298e2af3e746f6
emmaG0730 <xiaoyezi2006719@126.com>
2018-09-28 22:54:30 +0800

    proportion

--- code/armWithPysideUI.py
+++ code/armWithPysideUI.py
@@ -1,1 +1,49 @@
+import maya.cmds as mc
+import os
+#mc.select( all=True )
+#mc.delete()
+class ArmCreation(object):
+	use= None
+	@classmethod	
+	def showUI(cls,uiFile):
+		win = cls(uiFile)
+		win.create()
+		return win
+				
+	def __init__(self, filePath,*args):
+		"""Initialize data attributes"""
+		ArmCreation.use = self
+		self.window = 'arm'
+		self.inputField = 'input'
+		self.uiFile = filePath
+        
+	def create(self, verbose=False):   	
+		if mc.window(self.window, exists=True):
+		    mc.deleteUI(self.window)
+		self.window = mc.loadUI(uiFile=self.uiFile,verbose=verbose)
+		mc.showWindow(self.window)
+	def createArm():
+		print 'A arm!'
+win=ArmCreation(os.getcwd(),'arm.ui')
+win.create(verbose=False)
+
 print os.getcwd()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ code/proportion.py
@@ -0,0 +1,9 @@
+import maya.cmds as mc
+head= 5
+foot_angle = head*0.5
+angle_knee= head*1.5
+knee_pelvis = head*2
+pelvis_bellybutton= foot_angle
+bellybutton_neck = knee_pelvis
+neck_head= foot_angle
+head=head
