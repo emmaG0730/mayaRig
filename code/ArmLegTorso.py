d7eeb572e1fa1a9a47723ae5a7af707aff99ff2d
emmaG0730 <xiaoyezi2006719@126.com>
2018-10-22 17:42:28 +0800

    Revert "latest"
    This reverts commit c5e56b541695957ff6ad0b60d5d81a118a905351.

--- code/ArmLegTorso.py
+++ code/ArmLegTorso.py
@@ -1,66 +1,32 @@
 import maya.cmds as mc
 #create IK FK SKIN joints (arm)
-
 GeoLocList = []
 GeoDic = {0:'ShoulderLeft',1:'ElbowLeft',2:'WristLeft',3:'ThighLeft',
-			4:'KneeLeft',5:'AnkleLeft',6:'FootLeft',7:'ToeLeft',8:'Root',9:'Spine0',10:'Spine1',
-			11:'Spine2',12:'Spine3',13:'Neck',14:'Head',15:'HeadEnd',16:'Clavicle'}
+			4:'KneeLeft',5:'AnkleLeft',6:'FootLeft',7:'Root',8:'Spine0',9:'Spine1',
+			10:'Spine2',11:'Spine3',12:'Neck',13:'Head',14:'HeadEnd'}
 for each in (GeoDic[0],GeoDic[1],GeoDic[2],GeoDic[3],GeoDic[4],GeoDic[5],
-			GeoDic[6],GeoDic[7],GeoDic[8],GeoDic[9],GeoDic[10],GeoDic[11],GeoDic[12],
-			GeoDic[13],GeoDic[14],GeoDic[15],GeoDic[16]):
+			GeoDic[6],GeoDic[7],GeoDic[8],GeoDic[9],GeoDic[10],GeoDic[11],GeoDic[12],GeoDic[13],GeoDic[14]):
 	mc.select(each + 'Loc')
 	GeoT = mc.xform(q = True,t = True,ws = True)
 	GeoLocList.append(GeoT)
+	
 mc.select(clear=True)
-
-#new method create torso joints only one joint chain
-mc.select(clear=True)
-mc.joint( n = GeoDic[0]+'_JNT',p = (GeoLocList[0][0],GeoLocList[0][1],GeoLocList[0][2]), rad = 0.4)
-for i in range(1,17):
-	mc.joint( n = GeoDic[i]+'_JNT',p = (GeoLocList[i][0],GeoLocList[i][1],GeoLocList[i][2]), rad = 0.4)
-	mc.joint(GeoDic[i-1]+'_JNT',e= True, zso = True,oj='xyz',sao  ='yup')
-	if i == 2 or i == 7 or i == 15:
-		mc.setAttr(GeoDic[i]+'_JNT' + '.jointOrientZ',0)
-		mc.setAttr(GeoDic[i]+'_JNT' + '.jointOrientY',0)
-		mc.setAttr(GeoDic[i]+'_JNT' + '.jointOrientX',0)
-		mc.select(clear=True)
-		
-mc.select(GeoDic[1] + '_JNT')
+for JNT in ('_FK_JNT','_IK_JNT','_SkinJNT'):
+	mc.select(clear=True)
+	mc.joint( n = GeoDic[0]+JNT,p = (GeoLocList[0][0],GeoLocList[0][1],GeoLocList[0][2]), rad = 0.4)
+	for i in range(1,15):
+		mc.joint( n = GeoDic[i]+JNT,p = (GeoLocList[i][0],GeoLocList[i][1],GeoLocList[i][2]), rad = 0.4)
+		mc.joint(GeoDic[i-1]+JNT,e= True, zso = True,oj='xyz',sao  ='yup')
+		if i == 2 or i == 6:
+			mc.select(clear=True)		
+	mc.select(GeoDic[0]+JNT , GeoDic[1]+JNT ,GeoDic[2]+JNT )
+	mc.createDisplayLayer(noRecurse=True,n = JNT + '_Layer')		
+for i in range(3):	
+	mc.parentConstraint(GeoDic[i]+'_FK_JNT',GeoDic[i]+'_IK_JNT', GeoDic[i]+'_SkinJNT')
+mc.select(GeoDic[1] + '_IK_JNT')
 mc.joint(e= True , spa = True,ch = True)
-mc.ikHandle( sj=GeoDic[0]+'_JNT', ee=GeoDic[2]+'_JNT',sol = 'ikRPsolver')
-mc.rename('ikHandle1','WristIKhandle')		
-
-mc.select(GeoDic[4] + '_JNT')
-mc.joint(e= True , spa = True,ch = True)
-mc.ikHandle( sj=GeoDic[3]+'_JNT', ee=GeoDic[5]+'_JNT',sol = 'ikRPsolver')
-mc.rename('ikHandle1','AnkleIKhandle')
-
-#mc.select(GeoDic[4] + '_JNT')
-#mc.joint(e= True , spa = True,ch = True)
-mc.ikHandle( sj=GeoDic[5]+'_JNT', ee=GeoDic[6]+'_JNT',sol = 'ikSCsolver')
-mc.rename('ikHandle1','FootIKhandle')
-mc.ikHandle( sj=GeoDic[6]+'_JNT', ee=GeoDic[7]+'_JNT',sol = 'ikSCsolver')
-mc.rename('ikHandle1','ToeIKhandle')
-
-
-#shoulder
-mc.spaceLocator(n = 'Shoulder_orient_loc')
-mc.group('Shoulder_orient_loc' ,n = 'Shoulder_orient_loc_grp')
-mc.delete(mc.pointConstraint('ShoulderLeft_JNT','Shoulder_orient_loc_grp'))	
-mc.parent( GeoDic[0]+'_JNT' , 'Shoulder_orient_loc')
-mc.parent('Shoulder_orient_loc','Clavicle_JNT')
-mc.parent('Clavicle_JNT','Spine3_JNT')
-
-mc.delete(mc.duplicate('Shoulder_orient_loc',rc = True)[1:])
-mc.rename('Shoulder_orient_loc1','Body_orient_loc')
-mc.parent('Body_orient_loc','Root_JNT')
-
-mc.delete(mc.duplicate('Shoulder_orient_loc',rc = True)[1:])
-mc.rename('Shoulder_orient_loc1','Spine_orient_loc')
-mc.parent('Spine_orient_loc','Spine3_JNT')
-
-mc.orientConstraint('Body_orient_loc','Spine_orient_loc','Shoulder_orient_loc')
-
+mc.ikHandle( sj=GeoDic[0]+'_IK_JNT', ee=GeoDic[2]+'_IK_JNT',sol = 'ikRPsolver')
+mc.rename('ikHandle1','ArmIKhandle')
 
 #fingers joints
 FingerList = []
@@ -89,28 +55,20 @@ for i in range(1,3):
 FingerList=[]
 
 #create hand
-mc.delete(mc.duplicate('WristLeft_JNT',rc = True)[1:])
-mc.rename('WristLeft_JNT1','L_hand')
-mc.parent('L_hand',w = True)
-
+mc.select(clear=True)
+wirstT = mc.xform('WristLeft_SkinJNT',q = True,t = True,ws = True)
+mc.joint( p = (wirstT[0],wirstT[1],wirstT[2]))
+mc.joint(n = 'LeftHand',e= True, zso = True,oj='xyz',sao  ='yup', rad = 0.2)
 for each in ('ThumbLeft0','PinkieLeft0','RingLeft0','MiddleLeft0','IndexLeft0'):	
-	mc.parent(each,'L_hand')
-mc.pointConstraint('WristLeft_JNT','L_hand')
-mc.orientConstraint('WristLeft_JNT','L_hand')
-
-
-#torso splineIK ,didn't finished
-influences = ['Root_JNT', 'Spine3_JNT']
-
-mc.rename('curve1','spine_curve')
-mc.ikHandle(sj=influences[0], ee=influences[1] , sol = 'ikSplineSolver',ccv = False,c = 'spine_curve',roc = True)
-scls = mc.skinCluster(influences,'spine_curve', name='spine_skinCluster', toSelectedBones=True, 
-							bindMethod=1, skinMethod=0, normalizeWeights=1)[0]
+	mc.parent(each,'LeftHand')
+for each in ('Wrist_FK_JNT','Wrist_FK_JNT','Wrist_FK_JNT'):  
+	mc.pointConstraint(each ,'LeftHand')
+	mc.orientConstraint(each ,'LeftHand')
 
 	
-#hide body locators
-mc.ls(sl = True,type = 'locator')
-
+	
+	
+	
 	
 	
 	
--- code/controller.py
+++ code/controller.py
@@ -1,213 +1,131 @@
 import maya.cmds as mc
-#from body_joint import *
-#reload(body_joint)
-#joint_creation()
 
 #create FK controller and constraint
-for i in ('Shoulder','Elbow','Wrist','Thigh','Knee','Ankle','Foot','Toe'):
+for i in ('Shoulder','Elbow','Wrist'):
 	mc.circle(n = 'FK_Left' + i +'_CON',c = (0,0,0),r =1)
 	mc.group('FK_Left'+ i +'_CON',name ='FK_Left'+ i +'_CON_grp' )
-	mc.delete(mc.parentConstraint(i + 'Left_JNT','FK_Left'+ i +'_CON_grp',mo = False))
+	mc.parentConstraint(i + 'Left_FK_JNT','FK_Left'+ i +'_CON_grp',mo = False)
 	mc.setAttr('FK_Left' + i + '_CON.rotateY', 90)
 	mc.makeIdentity('FK_Left' + i + '_CON',apply = True)
-	mc.parentConstraint('FK_Left' + i + '_CON',i + 'Left_JNT')
+	mc.delete('FK_Left' + i + '_CON_grp_parentConstraint1')
+	mc.parentConstraint('FK_Left' + i + '_CON',i + 'Left_FK_JNT')
 mc.parent('FK_LeftWrist_CON_grp','FK_LeftElbow_CON')
 mc.parent('FK_LeftElbow_CON_grp','FK_LeftShoulder_CON')
-mc.parent('FK_LeftAnkle_CON_grp','FK_LeftKnee_CON')
-mc.parent('FK_LeftKnee_CON_grp','FK_LeftThigh_CON')
-mc.parent('FK_LeftFoot_CON_grp','FK_LeftKnee_CON')
-mc.parent('FK_LeftToe_CON_grp','FK_LeftFoot_CON')
+for j in ('tx','ty','tz','rx','rz','sx','sy','sz'):
+	mc.setAttr('FK_LeftElbow_CON.'+j,l = True,k =False)
+
 #create IK controller and IKFK switcher
 mc.file('F:\\GitHub\\tools\\mayaRig\\squreAndCubeIcon.ma',i = True)
+mc.select('FK_LeftWrist_CON')
+FKWristCON_t = mc.xform(q=True,ws=True,t=True)
+FKWristCON_r = mc.xform(q=True,ws=True,ro=True)
 mc.select('controller')
-mc.duplicate()
-mc.rename('controller1','IK_LeftWrist_CON')
-mc.group('IK_LeftWrist_CON',n = 'IK_LeftWrist_CON_grp' , r = True)
-mc.delete(mc.parentConstraint('FK_LeftWrist_CON', 'IK_LeftWrist_CON_grp'))
-mc.parent('WristIKhandle','IK_LeftWrist_CON') 
+mc.rename('controller','IK_LeftWrist_CON')
 mc.scale(1,0.5,1.2)
+mc.group('IK_LeftWrist_CON',n = 'IK_LeftWrist_CON_grp')
+mc.xform(ws=True,t=(FKWristCON_t[0],FKWristCON_t[1],FKWristCON_t[2]))
+mc.xform(ws=True,ro=(FKWristCON_r[0],FKWristCON_r[1],FKWristCON_r[2]))
+mc.parent('ArmIKhandle','IK_LeftWrist_CON') 
 
-#foot ik controller
-for each in ('Foot','Toe'):
-	mc.duplicate('controller')
-	mc.rename('controller1','IK_Left'+each+'_CON')
-	mc.group('IK_Left'+each+'_CON',n = 'IK_Left'+each+'_CON_grp')
-	mc.move(0,-0.5,0.5,'IK_Left'+each+'_CON_grp.scalePivot','IK_Left'+each+'_CON_grp.rotatePivot')
-	mc.delete(mc.pointConstraint(each+'Left_JNT','IK_Left'+each+'_CON_grp'))
-	mc.scale(1,0.5,1)
-mc.select('IK_LeftFoot_CON_grp')
-mc.scale(1,0.5,1.4)
-Ankle_t = mc.xform('AnkleLeft_JNT', t=1, ws=1, q=1)
-mc.move(Ankle_t[0],Ankle_t[1],Ankle_t[2],'IK_LeftFoot_CON.scalePivot','IK_LeftFoot_CON.rotatePivot')
-mc.parent('AnkleIKhandle','IK_LeftToe_CON_grp','FootIKhandle','ToeIKhandle','IK_LeftFoot_CON')
+mc.file('F:\\GitHub\\tools\\mayaRig\\squreAndCubeIcon.ma',i = True)
+mc.select('controller')
+mc.rename('controller','IKFKSwitch')
+mc.group('IKFKSwitch',n = 'IKFKSwitch_grp')
+mc.scale(0.6,0.02,0.6)
+mc.xform(ws=True,t=(FKWristCON_t[0],FKWristCON_t[1]+0.5,FKWristCON_t[2]))
+mc.xform(ws=True,ro=(FKWristCON_r[0],FKWristCON_r[1]+0.5,FKWristCON_r[2]))
+mc.parent('IKFKSwitch_grp','WristLeft_SkinJNT')
+mc.select('IKFKSwitch')
+mc.addAttr(ln = 'IKFKTrigger',at= 'enum',en = 'IK:FK',k=True)
 
-#Switcher arm
-for each in ('Wrist','Foot'):
-	mc.duplicate('controller')
-	mc.rename('controller1', each + 'IKFKSwitch')
-	mc.group(each + 'IKFKSwitch',n = each + 'Switch_grp')
-	mc.scale(0.6,0.02,0.6)
-	mc.delete(mc.parentConstraint('FK_Left'+each+'_CON', each + 'Switch_grp'))
-	mc.parent(each + 'Switch_grp',each + 'Left_JNT')
-	mc.move(0,1,0,ls = True)
-	mc.select(each + 'IKFKSwitch')
-	mc.addAttr(ln = each + 'IKFKTrigger',at= 'enum',en = 'IK:FK',k=True)
-	for item in ('tx','ty','tz','rx','ry','rz','sx','sy','sz','visibility'):
-		mc.setAttr(each + 'IKFKSwitch.'+item,l = True,k =False)
+#create pole/fake pole controller
+mc.file('F:\\GitHub\\tools\\mayaRig\\squreAndCubeIcon.ma',i = True)
+for each in ('Pole_CON','FakePole_CON'):	
+	mc.select('ElbowLeft_FK_JNT')
+	FKElbow_t = mc.xform(q=True,ws=True,t=True)
+	FKElbow_r = mc.xform(q=True,ws=True,ro=True)
+	mc.select('controller')
+	mc.duplicate()
+	mc.rename('controller1',each)
+	mc.group(each,n = each+'_grp')
+	mc.scale(0.4,0.4,0.4)
+	mc.xform(ws=True,t=(FKElbow_t[0],FKElbow_t[1],FKElbow_t[2]-2))
+	mc.xform(ws=True,ro=(FKElbow_r[0],FKElbow_r[1],FKElbow_r[2]-2))
+mc.poleVectorConstraint('Pole_CON','ArmIKhandle') 
+mc.parent('FakePole_CON_grp','FK_LeftElbow_CON')
 
+################################################################################
 
+triggerState = mc.getAttr("IKFKSwitch.IKFKTrigger")
+if triggerState == 1:
+	mc.setAttr ("Elbow_SKIN_JNT_parentConstraint1.Elbow_IK_JNTW1" ,0)
+	mc.setAttr ("Elbow_SKIN_JNT_parentConstraint1.Elbow_FK_JNTW0" ,1)
+	mc.setAttr ("Shoulder_SKIN_JNT_parentConstraint1.Shoulder_IK_JNTW1" ,0)
+	mc.setAttr ("Shoulder_SKIN_JNT_parentConstraint1.Shoulder_FK_JNTW0" ,1)	
+	mc.setAttr ("Wrist_SKIN_JNT_parentConstraint1.Wrist_IK_JNTW1" ,0)
+	mc.setAttr ("Wrist_SKIN_JNT_parentConstraint1.Wrist_FK_JNTW0" ,1)
+	'''
+	mc.setAttr('FK_Shoulder_CON.visibility',1)
+	mc.setAttr('FK_Elbow_CON.visibility',1)
+	mc.setAttr('FK_Wrist_CON.visibility',1)
+	mc.setAttr('IK_Wrist_CON.visibility',0)
+	mc.setAttr('Pole_CON.visibility',0)
+	'''
+	mc.select('Shoulder_IK_JNT')
+	Shoulder_IK_RO = mc.xform(q=True,ws=True,ro=True)
+	mc.select('FK_Shoulder_CON')
+	mc.xform(ws=True,ro=(Shoulder_IK_RO[0],Shoulder_IK_RO[1],Shoulder_IK_RO[2]))
 
-#create pole controller
-for each in ('Elbow','Knee'):
-	mc.delete(mc.duplicate(each + 'Left_JNT',rc = True)[1:])
-	mc.rename(each+'Left_JNT1',each+'Pole_CON')
-	mc.parent(each+'Pole_CON',w = True)
-	joint_t = mc.xform(each+'Left_JNT', t=1, ws=1, q=1)	
-	mc.group(each+'Pole_CON',n = each+'Pole_CON_grp')
-	if each == 'Elbow':
-		mc.move(0,0,-2,each+'Pole_CON_grp',ls = True)
-	elif each == 'Knee':
-		mc.move(0,0,2,each+'Pole_CON_grp',ls = True)
-	pole_t = mc.xform(each+'Pole_CON', t=1, ws=1, q=1)
-	#add a line between pole control and elbow
-	mc.curve(n=each+'_pole_line',p = [(0,0,0),(1,0,0)],d =1)
-	mc.select(each+'_pole_line.cv[0]')
-	mc.xform(ws=True,t=(joint_t[0],joint_t[1],joint_t[2]))
-	mc.select(each+'_pole_line.cv[1]')
-	mc.xform(ws=True,t=(pole_t[0],pole_t[1],pole_t[2]))
-	influences = [each+'Pole_CON', each+'Left_JNT']
-	mc.skinCluster(influences,each+'_pole_line', name=each+'_skinCluster', toSelectedBones=True,
-					bindMethod=1, skinMethod=0, normalizeWeights=1)[0]
-	mc.select(each+'_pole_line',each+'Pole_CON')
-	mc.setAttr(each+"Pole_CON.overrideEnabled",1)
-	mc.setAttr( each+"_pole_line.overrideColor", 0)
+	mc.select('Elbow_IK_JNT')
+	Elbow_IK_RO = mc.xform(q=True,ws=True,ro=True)
+	mc.select('FK_Elbow_CON')
+	mc.xform(ws=True,ro=(Elbow_IK_RO[0],Elbow_IK_RO[1],Elbow_IK_RO[2]))
+    
+	mc.select('Wrist_IK_JNT')
+	Wrist_IK_RO = mc.xform(q=True,ws=True,ro=True)
+	Wrist_IK_T = mc.xform(q=True,ws=True,t=True)
+	mc.select('FK_Wrist_CON')
+	mc.xform(ws=True,ro=(Wrist_IK_RO[0],Wrist_IK_RO[1],Wrist_IK_RO[2]))
+	mc.xform(ws=True,t=(Wrist_IK_T[0],Wrist_IK_T[1],Wrist_IK_T[2]))
+    
+	mc.select('Pole_CON')
+	Pole_CON_T = mc.xform(q=True,ws=True,t=True)
+	#Pole_CON_RO = mc.xform(q=True,ws=True,ro=True)
+    
+	mc.select('fkPole')
+	mc.xform(ws=True,t=(Pole_CON_T[0],Pole_CON_T[1],Pole_CON_T[2]))
+	#mc.xform(ws=True,ro=(Pole_CON_RO[0],Pole_CON_RO[1],Pole_CON_RO[2]))
+
 	
-mc.poleVectorConstraint('ElbowPole_CON','WristIKhandle') 
-mc.poleVectorConstraint('KneePole_CON','AnkleIKhandle') 
-
-###################################################################################################
-
-#one joint chain IKFK switch(ARM)
-# From IK to FK
-if mc.getAttr("WristIKFKSwitch.WristIKFKTrigger"):             
-	mc.delete(mc.orientConstraint("ShoulderLeft_JNT", "FK_LeftShoulder_CON"))
-	mc.delete(mc.parentConstraint("WristLeft_JNT", "FK_LeftWrist_CON"))
-	mc.setAttr ("WristLeft_JNT_parentConstraint1.FK_LeftWrist_CONW0" ,1)
-	mc.setAttr ("ElbowLeft_JNT_parentConstraint1.FK_LeftElbow_CONW0" ,1)
-	mc.setAttr ("ShoulderLeft_JNT_parentConstraint1.FK_LeftShoulder_CONW0" ,1)
-	mc.setAttr('WristIKhandle.ikBlend', 0)
- 
-else:
-    # From FK to IK
-	mc.delete(mc.parentConstraint("FK_LeftWrist_CON", 'IK_LeftWrist_CON'))
-	arm01Vec = [mc.xform("ElbowLeft_JNT", t=1, ws=1, q=1)[i] - mc.xform("ShoulderLeft_JNT", t=1, ws=1, q=1)[i] for i in range(3)]
-	arm02Vec = [mc.xform("ElbowLeft_JNT", t=1, ws=1, q=1)[i] - mc.xform("WristLeft_JNT", t=1, ws=1, q=1)[i] for i in range(3)]
-	mc.xform("ElbowPole_CON", t=[mc.xform("ElbowLeft_JNT", t=1, q=1, ws=1)[i] + arm01Vec[i] * .75 + arm02Vec[i] * .75 for i in range(3)], ws=1)
-	mc.setAttr('WristIKhandle.ikBlend', 1)
-				
-	mc.setAttr ("WristLeft_JNT_parentConstraint1.FK_LeftWrist_CONW0" ,0)
-	mc.setAttr ("ElbowLeft_JNT_parentConstraint1.FK_LeftElbow_CONW0" ,0)
-	mc.setAttr ("ShoulderLeft_JNT_parentConstraint1.FK_LeftShoulder_CONW0" ,0)
-
-######################################################################################################
-
-#one joint chain IKFK switch(LEG)
-# From IK to FK
-if mc.getAttr("FootIKFKSwitch.FootIKFKTrigger"):             
-	mc.delete(mc.orientConstraint("ThighLeft_JNT", "FK_LeftThigh_CON"))
-	mc.delete(mc.parentConstraint("AnkleLeft_JNT", "FK_LeftAnkle_CON"))
-	mc.setAttr ("ThighLeft_JNT_parentConstraint1.FK_LeftThigh_CONW0" ,1)
-	mc.setAttr ("KneeLeft_JNT_parentConstraint1.FK_LeftKnee_CONW0" ,1)
-	mc.setAttr ("AnkleLeft_JNT_parentConstraint1.FK_LeftAnkle_CONW0" ,1)
-	mc.setAttr('AnkleIKhandle.ikBlend', 0)
-	mc.setAttr('FootIKhandle.ikBlend', 0)
-	mc.setAttr('ToeIKhandle.ikBlend', 0)
+elif triggerState == 0:
+	mc.setAttr ("Elbow_SKIN_JNT_parentConstraint1.Elbow_IK_JNTW1" ,1)
+	mc.setAttr ("Elbow_SKIN_JNT_parentConstraint1.Elbow_FK_JNTW0" ,0)
+	mc.setAttr ("Shoulder_SKIN_JNT_parentConstraint1.Shoulder_IK_JNTW1" ,1)
+	mc.setAttr ("Shoulder_SKIN_JNT_parentConstraint1.Shoulder_FK_JNTW0" ,0)	
+	mc.setAttr ("Wrist_SKIN_JNT_parentConstraint1.Wrist_IK_JNTW1" ,1)
+	mc.setAttr ("Wrist_SKIN_JNT_parentConstraint1.Wrist_FK_JNTW0" ,0)	
+	'''
+	mc.setAttr('FK_Shoulder_CON.visibility',0)
+	mc.setAttr('FK_Elbow_CON.visibility',0)
+	mc.setAttr('FK_Wrist_CON.visibility',0)
+	mc.setAttr('IK_Wrist_CON.visibility',1)
+	mc.setAttr('Pole_CON.visibility',1)
+	'''
 	
- 
-else:
-    # From FK to IK
-	mc.delete(mc.pointConstraint("FK_LeftAnkle_CON", 'IK_LeftFoot_CON'))
-	arm01Vec = [mc.xform("KneeLeft_JNT", t=1, ws=1, q=1)[i] - mc.xform("ThighLeft_JNT", t=1, ws=1, q=1)[i] for i in range(3)]
-	arm02Vec = [mc.xform("KneeLeft_JNT", t=1, ws=1, q=1)[i] - mc.xform("AnkleLeft_JNT", t=1, ws=1, q=1)[i] for i in range(3)]
-	mc.xform("KneePole_CON", t=[mc.xform("KneeLeft_JNT", t=1, q=1, ws=1)[i] + arm01Vec[i] * .75 + arm02Vec[i] * .75 for i in range(3)], ws=1)
-	mc.setAttr('AnkleIKhandle.ikBlend', 1)
-	mc.setAttr('FootIKhandle.ikBlend', 1)
-	mc.setAttr('ToeIKhandle.ikBlend', 1)
-			
-	mc.setAttr ("ThighLeft_JNT_parentConstraint1.FK_LeftThigh_CONW0" ,0)
-	mc.setAttr ("KneeLeft_JNT_parentConstraint1.FK_LeftKnee_CONW0" ,0)
-	mc.setAttr ("AnkleLeft_JNT_parentConstraint1.FK_LeftAnkle_CONW0" ,0)
-	
-#########################################################################################################
+	mc.select('FK_Wrist_CON')
+	FK_WristP= mc.xform(q=True,ws=True,t=True)
+	FK_WristRO= mc.xform(q=True,ws=True,ro=True)	
+	mc.select('IK_Wrist_CON')
+	mc.xform(ws=True,t=(FK_WristP[0],FK_WristP[1],FK_WristP[2]))
+	mc.xform(ws=True,ro=(FK_WristRO[0],FK_WristRO[1],FK_WristRO[2]))	
+	mc.select('fkPole')
+	fkPoleT=mc.xform(q=True,ws=True,t=True)
+	fkPoleRO=mc.xform(q=True,ws=True,ro=True)	
+	mc.select('Pole_CON')
+	mc.xform(ws=True,t=(fkPoleT[0],fkPoleT[1],fkPoleT[2]))
+	mc.xform(ws=True,ro=(fkPoleRO[0],fkPoleRO[1],fkPoleRO[2]))
 
-
-#bodycontroller
-#mc.duplicate('controller')
-#mc.rename('controller1','Spine0_con')
-mc.circle(n = 'Spine0_con',r = 2)
-mc.group('Spine0_con',n = 'Spine0_con_grp')
-mc.delete(mc.parentConstraint('Spine0_JNT','Spine0_con_grp'))
-mc.scale(0.5,1,1)
-mc.makeIdentity('Spine0_con',apply = True)
-
-
-mc.duplicate('Spine1_con')
-mc.rename('Spine1_con','Spine3_con')
-mc.group('Spine3_con',n = 'Spine3_con_grp')
-mc.delete(mc.parentConstraint('Spine3_JNT','Spine3_con_grp'))
-
-mc.delete(mc.duplicate('Spine3_JNT',rc = True)[1:])
-mc.rename('Spine3_JNT1','spine3_con_joint')
-mc.parent('spine3_con_joint',w = True)
-
-mc.delete(mc.duplicate('Root_JNT',rc = True)[1:])
-mc.rename('Root_JNT1','Root_con_joint')
-mc.parent('Root_con_joint',w = True)
-
-mc.parent('spine3_con_joint','Spine3_con')
-mc.parent('Root_con_joint','Spine1_con')
-
-#whole body controller
-mc.circle(n = 'body_CON',r = 4)
-mc.setAttr('body_CON.rotateX',-90)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+###################################################################################
 
 
 
